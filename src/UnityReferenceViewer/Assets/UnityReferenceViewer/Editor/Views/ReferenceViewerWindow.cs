41229579c8107289e352510e3ed5a7be59a70484
Ichiro Dohi <idohi@live.jp>
2023-05-22 04:39:38 +0900

    Add toggle and cache

--- src/UnityReferenceViewer/Assets/UnityReferenceViewer/Editor/Views/ReferenceViewerWindow.cs
+++ src/UnityReferenceViewer/Assets/UnityReferenceViewer/Editor/Views/ReferenceViewerWindow.cs
@@ -9,6 +9,7 @@ https://opensource.org/licenses/mit-license.php
 
 #nullable enable
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
@@ -22,6 +23,8 @@ namespace UnityReferenceViewer.Editor.Views
 	{
 		private const string HeaderLabelTitle = "【Find References In Project - Result】";
 
+		private readonly Dictionary<string, SearchItem> _searchCache = new();
+
 		public static void CreateWindow(ReferenceFinder finder, SearchResult result)
 		{
 			var window = GetWindow<ReferenceViewerWindow>("ReferenceViewer2");
@@ -32,6 +35,8 @@ namespace UnityReferenceViewer.Editor.Views
 		private string? _additionalInformation;
 		private IReadOnlyList<SearchResultView>? _resultViews;
 		private Vector2 _scrollPosition = Vector2.zero;
+
+		private bool _automaticUpdate;
 
 		public void SetResult(ReferenceFinder finder, SearchResult result)
 		{
@@ -56,10 +61,19 @@ namespace UnityReferenceViewer.Editor.Views
 
 		private void OnSelectionUpdate()
 		{
-			Debug.Log("changed");
-			if (_referenceFinder == null)
+			if (_automaticUpdate == false)
 			{
 				return;
+			}
+			
+			if (_referenceFinder == null)
+			{
+				#if UNITY_EDITOR_OSX
+				var ignore = ArraySegment<string>.Empty;
+				_referenceFinder = new ReferenceFinder(new MacOsGrepReferenceFinder(ignore), ignore);
+				#elif UNITY_EDITOR_WIN
+				_referenceFinder = new ReferenceFinder(new WindowsFindStrReferenceFinder(), ArraySegment<string>.Empty);
+				#endif
 			}
 			
 			var selection = Selection.assetGUIDs;
@@ -68,17 +82,44 @@ namespace UnityReferenceViewer.Editor.Views
 				return;
 			}
 			
-			var itemsResults = selection.Select(_referenceFinder.FindReferencedFiles);
+			var itemsResults = selection.Select(
+				guid =>
+				{
+					var result = _searchCache.GetValueOrDefault(guid);
+					if (result != null)
+					{
+						return result;
+					}
+					
+					result = _referenceFinder.FindReferencedFiles(guid);
+					_searchCache.Add(guid, result);
+					return result;
+				});
 			var searchResult = new SearchResult(itemsResults.ToArray(), _additionalInformation);
 			SetResult(_referenceFinder, searchResult);
 		}
 
 		private void OnGUI()
 		{
+			using (new EditorGUILayout.HorizontalScope())
+			{
+				var text = _automaticUpdate ? "Disable Auto Update" : "Enable Auto Update";
+				if (GUILayout.Button(text))
+				{
+					_automaticUpdate = !_automaticUpdate;
+				}
+				if (GUILayout.Button("Clear cache"))
+				{
+					_searchCache.Clear();
+					OnSelectionUpdate();
+				}
+			}
+			
 			if (_resultViews == null)
 			{
 				return;
 			}
+			
 			EditorGUILayout.Separator();
 			GUILayout.Label(HeaderLabelTitle);
 
