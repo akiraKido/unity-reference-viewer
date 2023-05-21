129841183c7a41d4d8dcb53ecde41a07fd9c0b5f
Ichiro Dohi <idohi@live.jp>
2023-05-22 04:18:36 +0900

    Update build

--- builds/Editor/Views/ReferenceViewerWindow.cs
+++ builds/Editor/Views/ReferenceViewerWindow.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityReferenceViewer.Editor.Models;
 using UnityReferenceViewer.Editor.ValueObjects;
 
 namespace UnityReferenceViewer.Editor.Views
@@ -21,23 +22,55 @@ namespace UnityReferenceViewer.Editor.Views
 	{
 		private const string HeaderLabelTitle = "【Find References In Project - Result】";
 
-		public static void CreateWindow(SearchResult result)
+		public static void CreateWindow(ReferenceFinder finder, SearchResult result)
 		{
 			var window = GetWindow<ReferenceViewerWindow>("ReferenceViewer2");
-			window.SetResult(result);
+			window.SetResult(finder, result);
 		}
 		
+		private ReferenceFinder? _referenceFinder;
 		private string? _additionalInformation;
 		private IReadOnlyList<SearchResultView>? _resultViews;
 		private Vector2 _scrollPosition = Vector2.zero;
 
-		public void SetResult(SearchResult result)
+		public void SetResult(ReferenceFinder finder, SearchResult result)
 		{
+			_referenceFinder = finder;
+			
 			_resultViews = result.SearchResults.Select(it => new SearchResultView(it)).ToArray();
 			_additionalInformation = result.AdditionalInformation;
 			
 			_scrollPosition = Vector2.zero;
 			Repaint();
+		}
+
+		private void OnEnable()
+		{
+			Selection.selectionChanged += OnSelectionUpdate;
+		}
+
+		private void OnDisable()
+		{
+			Selection.selectionChanged -= OnSelectionUpdate;
+		}
+
+		private void OnSelectionUpdate()
+		{
+			Debug.Log("changed");
+			if (_referenceFinder == null)
+			{
+				return;
+			}
+			
+			var selection = Selection.assetGUIDs;
+			if (selection.Length == 0)
+			{
+				return;
+			}
+			
+			var itemsResults = selection.Select(_referenceFinder.FindReferencedFiles);
+			var searchResult = new SearchResult(itemsResults.ToArray(), _additionalInformation);
+			SetResult(_referenceFinder, searchResult);
 		}
 
 		private void OnGUI()
